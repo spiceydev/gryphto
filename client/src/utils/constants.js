39557fcdfb4cca21d72aaee9717a6f6cdeed390e
spiceydev <spiceydev@gmail.com>
2022-01-03 05:55:22 +1300

    smart contract deployed

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './transactions.json';
+
+export const contractAddress = '0xb63514C4f557FDA693134442102feF30Ae5eb261';
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: process.env.URL,
+      accounts: [process.env.META_KEY],
+    },
+  },
+};
